a12d549171468fd6f0f83e51bf30660b17944b1d
Leo Siniša Radošić <leo.radosic@gmail.com>
2018-10-17 22:41:33 +0200

    Prvi commit, sa base klasom.

---
+++ cmykui/src/main/java/com/cmykui/framework/cmykui/ANDROIDSTUDIOJEGLUPIOVAKLASASLUZIKAKOBIVIDIOFOLDERE.java
@@ -0,0 +1,4 @@
+package com.cmykui.framework.cmykui;
+
+public final class ANDROIDSTUDIOJEGLUPIOVAKLASASLUZIKAKOBIVIDIOFOLDERE {
+}
---
+++ cmykui/src/main/java/com/cmykui/framework/cmykui/base/CMYKBase.java
@@ -0,0 +1,5 @@
+package com.cmykui.framework.cmykui.base;
+
+public abstract class CMYKBase extends Object {
+
+}
