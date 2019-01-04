f0e77b9f6cc491544eab4644a22b74dbd13aa6a3
matcmrk <matija.cmrk@gmail.com>
2019-01-04 18:46:36 +0100

    Barchart linije i labele
    dodane labele ispod stupaca i usklađen je pravilni razmak linija

--- cmykui/src/main/java/com/cmykui/framework/cmykui/graph/BarChart.java
+++ cmykui/src/main/java/com/cmykui/framework/cmykui/graph/BarChart.java
@@ -4,7 +4,9 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.util.AttributeSet;
+import android.util.TypedValue;
 import android.view.View;
 
 
@@ -16,8 +18,11 @@ public class BarChart extends View {
     private Paint paint;
     private int width, height, padding;
     private boolean isInit = false;
+    private Rect rect = new Rect();
+    private int fontSize;
 
     public float[] Data = new float[7];
+    public String[] label = new String[7];
 
     public BarChart(Context context) {
         super(context);
@@ -38,9 +43,10 @@ public class BarChart extends View {
 
         width = getWidth();
         height = getHeight();
-        padding = 20;
+        padding = 30;
 
         isInit = true;
+        fontSize = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 13, getResources().getDisplayMetrics());
 
 
         Data[0] = 15.5f;
@@ -50,6 +56,15 @@ public class BarChart extends View {
         Data[4] = 25.5f;
         Data[5] = 8.5f;
         Data[6] = 25.5f;
+
+        label [0] = "prvi";
+        label [1] = "drugi";
+        label [2] = "treci";
+        label [3] = "cetvrti";
+        label [4] = "peti";
+        label [5] = "sesti";
+        label [6] = "sedmi";
+
 
     }
 
@@ -64,6 +79,7 @@ public class BarChart extends View {
         drawOsi(canvas);
         drawLines(canvas);
         drawShapes(canvas);
+        drawLabel(canvas);
     }
 
     private void drawShapes(Canvas canvas) {
@@ -108,21 +124,12 @@ public class BarChart extends View {
         paint.setStyle(Paint.Style.STROKE);
         paint.setAntiAlias(true);
 
-        int pomicanje = (int)Math.ceil(maxVisina());
+        int broj_linijaY = (int)Math.floor(maxVisina());
 
-        //int broj_linijaX = (int)Math.floor((width - padding*2) / pomicanje); // 10 je ramak izmedu linija
-        int broj_linijaY = (int)Math.round((height - padding) / pomicanje); // 10 je ramak izmedu linija
-
-        // po X
-        /*for(int i = 1; i <= broj_linijaX; i++){
-            int x = padding + (pomicanje * i);
-            int sY = height - padding; // sjeti se da je dole - 50;
-            int eY = height - 60;
-            canvas.drawLine(x, sY, x, eY, paint);
-        }*/
+        int pomicanje = Math.round((height - padding) / broj_linijaY);
 
         // po Y
-        for(int i = 1; i <= broj_linijaY; i++){
+        for(int i = 1; i < broj_linijaY; i++){
             int y = (height - padding) - (pomicanje * i);
             int sX = padding;
             int eX = padding + 10;
@@ -151,6 +158,23 @@ public class BarChart extends View {
         canvas.drawLine(startX, startY, startX, endY, paint);
 
     }
+
+    private void drawLabel(Canvas canvas) {
+        paint.setTextSize(fontSize);
+        paint.setColor(Color.WHITE);
+
+        float x = padding * 2;
+        float y = height - 5;
+        int maxWidth = (width-padding*2) / Data.length - (padding);
+        for (int i = 0; i < Data.length; i++){
+            paint.getTextBounds(label[i],0,label[i].length(),rect);
+
+            canvas.drawText(label[i],x,y,paint);
+
+            x += padding + maxWidth;
+        }
+    }
+
     private int randomColor(){
         Random rnd = new Random();
         int color = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
