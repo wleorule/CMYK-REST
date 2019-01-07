0586a986fcc459984135596883d07de2cce89cb8
Leo Siniša Radošić <leo.radosic@gmail.com>
2019-01-07 18:06:50 +0100

    Revert "Mcmrk"

---
+++ cmykui/src/main/java/com/cmykui/framework/cmykui/activity/BaseActivity.java
@@ -0,0 +1,9 @@
+package com.cmykui.framework.cmykui.activity;
+
+import android.app.Activity;
+
+import com.cmykui.framework.cmykui.base.CMYKBase;
+
+public abstract class BaseActivity extends Activity implements CMYKBase {
+    
+}
--- cmykui/src/main/java/com/cmykui/framework/cmykui/base/LayoutInterface.java
+++
@@ -1,9 +0,0 @@
-package com.cmykui.framework.cmykui.base;
-
-import android.view.View;
-
-public interface LayoutInterface {
-
-    //View.OnClickListener onClickListener(View v);
-
-}
